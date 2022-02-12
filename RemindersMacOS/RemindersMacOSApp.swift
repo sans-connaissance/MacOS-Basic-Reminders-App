2eda813c1793ca2dfd0886f2b8f784e5fcbe99ca
sans-connaissance <davemalicke@gmail.com>
2022-02-12 10:01:42 -0500

    Initial Commit

---
+++ RemindersMacOS/RemindersMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersMacOSApp.swift
+//  RemindersMacOS
+//
+//  Created by David Malicke on 2/12/22.
+//
+
+import SwiftUI
+
+@main
+struct RemindersMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
